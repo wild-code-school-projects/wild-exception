96253bb869f2effa39eb98bbf625ef611892c5bc
SuperCoolNinja <sbpro570@gmail.com>
2024-06-10 09:49:16 +0200

    init

---
+++ CustomException.cs
@@ -0,0 +1,14 @@
+﻿namespace Exceptions
+{
+    internal class CustomException : Exception
+    {
+
+        private readonly DateTime _dateTime;
+
+        public CustomException(string message) : base(message)
+        {
+            _dateTime = DateTime.Now;
+            Console.WriteLine($"{_dateTime.Hour}:{_dateTime.Minute}:{_dateTime.Second} - {message}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int value = Int32.Parse("abc");
+                Console.WriteLine(value);
+            }
+            catch (FormatException)
+            {
+                throw new CustomException("Invalid input: The input is not a valid integer.");
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+    }
+}
